6e1b7cec2152c025fd71cb3949f2e1640a421de1
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-06-19 13:59:57 +0700

    Refactor

--- Source/EasySyncClient/Client.fs
+++ Source/EasySyncClient/Client.fs
@@ -13,9 +13,55 @@ open WebDAVClient
 open System.Threading.Tasks
 open NLog
 
+module List = 
+    let apply (fl : ('a -> 'b) list) (xl : 'a list) =
+        [ for f in fl do 
+            for x in xl do
+                yield f x ]
+
+let inline (<*>) f xs = List.apply f xs
+
 let splitWith (sep: string) (str: string) = str.Split([| sep |], StringSplitOptions.None)
 let cleanPath (str: string) = str.TrimEnd('/').Replace("//", "/").Replace("\\", "/")
 let toSections (data: string array) = [ for i in 0..data.Length do yield String.concat "/"  <| data.Take(i) ]
+let replace (a:string) b (c: string)= c.Replace(a, b)
+
+type RalativeRemotePath = RelativeRemotePath of string
+type RelativeLocalPath = RelativeLocalPath of string
+
+type FullRemotePath = FullRemotePath of string
+type FullLocalPath = FullLocalPath of string
+
+type RemoteSection = RemoteSection of string array
+type RemoteRoot = RemoteRoot of string
+type LocalRoot = LocalRoot of string
+
+let createRelativePath (LocalRoot localRoot) (FullLocalPath localPath) = 
+    let relative = replace localRoot "" localPath 
+    let path = Path.GetDirectoryName relative
+    let name = Path.GetFileName relative 
+
+    printfn "%s" relative
+    printfn "%s" name
+    printfn "%s"  path
+
+    relative |> RelativeRemotePath
+
+let createSection (RelativeRemotePath path) =
+    let path = path |> cleanPath
+    splitWith "/" path |> toSections |> List.map RelativeRemotePath
+
+let createFullRemotePath (RemoteRoot root) (RelativeRemotePath path)  = 
+    root + "/" + path |> cleanPath
+
+let extractName (FullRemotePath path) = 
+    let last = path |> splitWith "/" |> Array.last
+    let path = Path.GetDirectoryName path
+    (path, last)
+
+type Action  = 
+    | CreateDir of RemoteRoot * RalativeRemotePath
+    | CreateFile of RalativeRemotePath * RelativeLocalPath
 
 type AlfrescoClient(endPoint) = 
     let logger = LogManager.GetCurrentClassLogger()
@@ -24,8 +70,14 @@ type AlfrescoClient(endPoint) =
     do 
         client.Server <- endPoint.Url
 
-    member this.TryCreateDirectory root section = 
-        let full = root + "/" + section |> cleanPath
+    member this.ProcessAction = function
+        | CreateDir (remoteRoot, remotePath) ->
+            this.TryCreateDirectory remoteRoot remotePath
+        | _ -> false
+
+    member this.TryCreateDirectory remoteRoot remotePath = 
+        let full = createFullRemotePath remoteRoot remotePath 
+
         async {
             do! Task.Delay(1000) |> Async.AwaitTask
             try 
@@ -34,8 +86,7 @@ type AlfrescoClient(endPoint) =
                 return true
             with ex ->
                 try 
-                    let last = full |> splitWith "/" |> Array.last
-                    let path = Path.GetDirectoryName full
+                    let path, last = extractName (FullRemotePath full)
                     logger.Info("Create {0} => {1}", path, last)
                     client.CreateDir(path, last) |> Async.AwaitTask |> ignore
                     return true
@@ -43,37 +94,33 @@ type AlfrescoClient(endPoint) =
                     return false
         } |> Async.RunSynchronously
 
-    member this.TryCreateDirectories root section =
+    member this.TryCreateDirectories remoteRoot remotePath  =
         async {
-            let full = root + "/" + section |> cleanPath
+            let full = createFullRemotePath remoteRoot remotePath
             try 
                 let! item = client.GetFolder full |> Async.AwaitTask
                 logger.Info("{0} is exists", full)
                 return true
             with ex ->
-                let sections = section |> splitWith "/" |> toSections
-                let rootMap = this.TryCreateDirectory root
+                let sections = createSection remotePath 
+                let rootMap = this.TryCreateDirectory remoteRoot
                 sections |> List.map rootMap |> ignore
                 return true
         } |> Async.RunSynchronously
 
-    member this.UploadFile (remoteRoot: string) (localRoot: string) (localFile: string) = 
-        let replace (a:string) b (c: string)= c.Replace(a, b)
+    member this.UploadFile (RemoteRoot remoteRoot) (LocalRoot localRoot) (FullLocalPath localPath) = 
 
-        if localFile.Contains localRoot then
-            let relative = replace localRoot "" localFile 
-            let path = Path.GetDirectoryName relative
-            let name = Path.GetFileName relative 
-
-            printfn "%s" relative
-            printfn "%s" name
-            printfn "%s"  path
-
-            this.TryCreateDirectories remoteRoot path |> ignore
-            let targetPath = remoteRoot + "/" + path |> cleanPath
+        if localPath.Contains localRoot then
 
             async {
-                let! item =  client.Upload(targetPath, File.OpenRead(localFile), name) |> Async.AwaitTask
+                let path = createRelativePath (LocalRoot localRoot) (FullLocalPath localPath)
+                let remote = RemoteRoot remoteRoot
+
+                this.TryCreateDirectories (RemoteRoot remoteRoot) path |> ignore
+                let targetPath = createFullRemotePath remote path
+                let name = Path.GetFileName localPath
+
+                let! item =  client.Upload(targetPath, File.OpenRead(localPath), name) |> Async.AwaitTask
                 return item
             }
             |> Async.RunSynchronously
--- Source/Tests/AlfrescoClientTests.fsx
+++ Source/Tests/AlfrescoClientTests.fsx
@@ -7,15 +7,20 @@ open EasySyncClient.Client
 let client = AlfrescoClient( { User = "admin"; Password = "admin"; Url = "http://192.168.0.109:8080"})
 
 let baseDir = "/alfresco/webdav/Validate"
+let root = RemoteRoot baseDir
 
 let createDir() =
-    client.TryCreateDirectories baseDir  "SecA/SecB/SecC" |> printfn "%A"
+    let relative = RelativeRemotePath "SecA/SecB/SecC"
+    client.TryCreateDirectories root  relative |> printfn "%A"
 
 let uploadFile() =
     let localFile = "Resource/Dir1/Dir2/Dir3/Test1.txt"
     let localPath = "Resource"
 
-    client.UploadFile baseDir localPath localFile
+    let localRoot = LocalRoot localPath
+    let fullPath = FullLocalPath localFile
 
+    client.UploadFile root localRoot fullPath
 
 uploadFile() |> printfn "%A"
+//createDir()
