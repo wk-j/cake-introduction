3392daf8b3a21ae872a70bf0555a2c284f14e1eb
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-06-21 17:12:09 +0700

    Test upload manager

--- Source/EasySyncClient/UploadManager.fs
+++ Source/EasySyncClient/UploadManager.fs
@@ -4,35 +4,54 @@ open EasySyncClient.AlfrescoClient
 open EasySyncClient.ClientModels
 open EasySyncClient.Models
 open EasySyncClient.DB
+open EasySyncClient.Utility
 open System
+open System.Timers
 
-let processFile (client: AlfrescoClient) (folder)  file = 
+module UploadManager = 
 
-    let action : FileAction = file.FileAction
-    let localPath = file.OriginalPath
-    let fullLocalPath = FullLocalPath localPath
-    let remoteRoot = RemoteRoot folder.RemotePath
-    let localRoot = LocalRoot folder.LocalPath
+    let private processFile (client: AlfrescoClient) (folder)  file = 
+        log "process file %s" file.OriginalPath
 
-    let results =
-        match action with
-        | FileAction.Created ->
-            client.UploadFile remoteRoot localRoot fullLocalPath
-        | FileAction.Deleted ->
-            client.DeleteFile remoteRoot localRoot fullLocalPath
-        | FileAction.Changed ->
-            client.UploadFile remoteRoot localRoot fullLocalPath
-        | FileAction.Renamed 
-        | FileAction.Moved ->
-            let info = { MoveInfo.OldPath = localPath; NewPath = file.NewPath }
-            client.MoveFile remoteRoot localRoot info
-        | _ -> false
-    (results)
+        let action : FileAction = file.FileAction
+        let localPath = file.OriginalPath
+        let fullLocalPath = FullLocalPath localPath
+        let remoteRoot = RemoteRoot folder.RemotePath
+        let localRoot = LocalRoot folder.LocalPath
 
-let start settings (folder: SyncFolder) =
+        let success =
+            match action with
+            | FileAction.Created ->
+                client.UploadFile remoteRoot localRoot fullLocalPath
+            | FileAction.Deleted ->
+                client.DeleteFile remoteRoot localRoot fullLocalPath
+            | FileAction.Changed ->
+                client.UploadFile remoteRoot localRoot fullLocalPath
+            | FileAction.Renamed 
+            | FileAction.Moved ->
+                let info = { MoveInfo.OldPath = localPath; NewPath = file.NewPath }
+                client.MoveFile remoteRoot localRoot info
+            | _ -> false
 
-    let alfresco = AlfrescoClient(settings)
-    let action = processFile alfresco folder
+        if success then
+            let newFile = { file with Status = Status.ProcessSuccess }
+            DbManager.updateFile newFile
+        else
+            let newFile = { file with Status = Status.ProcessFailed }
+            DbManager.updateFile newFile
 
-    let file = DbManager.queryFile Status.Initialize DateTime.MinValue
-    file |> Seq.toList |> List.map action
+    let start settings (folder: SyncFolder) =
+        let alfresco = AlfrescoClient(settings)
+        let action = processFile alfresco folder
+
+        let timer = new Timer(3000.0)
+
+        let handler x = 
+            log "query files ..."
+            let file = DbManager.queryFile Status.Initialize DateTime.MinValue
+            let rs = file |> Seq.toList |> List.map action
+            timer.Start()
+
+        timer.AutoReset <- false
+        timer.Enabled <- true
+        timer.Elapsed.Add(handler)
---
+++ Source/Tests/UploadManagerTests.fsx
@@ -0,0 +1,22 @@
+#r "../../Source/EasySyncClient/bin/Debug/EasySyncClient.dll"
+
+open EasySyncClient.UploadManager
+open EasySyncClient.Models
+open System
+
+
+let start() =
+    let config = {
+        EndPoint.Url = "http://192.168.0.109:8080"
+        User = "admin"
+        Password =" admin" }
+
+    let folder = {
+        SyncFolder.LocalPath = "Source"
+        RemotePath = "/alfresco/webdav/Validate"
+    }
+
+    UploadManager.start config folder
+    Console.ReadLine()
+
+start()
